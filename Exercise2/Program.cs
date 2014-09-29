c891756077107f8b3590e80cd3e969c7ec3f1149
JEBurgman <jan-eric.burgman.win14@utb.hermods.se>
2014-09-29 15:58:24 +0200

    New project

---
+++ Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fields
+            float a = 1F;
+            float b = 0.1F;
+            Console.WriteLine(a - b * 10);
+
+            decimal c = 1M;
+            decimal d = 0.1M;
+            Console.WriteLine(c - d * 10);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        /// <summary>
+        /// // This is the main entry point
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+            Console.ReadKey();
+        }
+    }
+}
