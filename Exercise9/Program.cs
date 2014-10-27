417764c07ccf7860e50cc57aaa07c8854591170d
JEBurgman <jan-eric.burgman.win14@utb.hermods.se>
2014-10-27 13:42:22 +0100

    Exercise 9 commited

--- Exercise9/Program.cs
+++ Exercise9/Program.cs
@@ -8,42 +8,133 @@ namespace Exercise9
 {
     class Program
     {
+        // Make the registry
+        static Registry registry = new Registry();
+
+        // Make a logger
+        static Logger log = new Logger();
+
         static void Main(string[] args)
         {
-            // Make the registry
-            var registry = new Registry();
+            while (true)
+            {
+                Console.Clear();
 
-            // Add a new employee to the registry
-            var newEmployee = new Employee("Kalle", "Anka", "550123-0123", "5000 kr/v");
+                // Print menu
+                MenuPrint();
 
-            var employeeList = registry.Employees;
+                // Make a choice
+                MakeChoice();
 
-            employeeList.Add(newEmployee);
+                Console.ReadKey();
+            }
+        }
 
-           
+        private static void MakeChoice()
+        {
+            var choice = int.Parse(Console.ReadLine());
 
-            // Add another employee
-            var anotherEmployee=new Employee("Kajsa", "Anka", "551011-7890","6000 kr/v");
+            switch (choice)
+            {
+                case 1:
+                    {
+                        AddEmployee();
+                        break;
+                    }
+                case 2:
+                    {
+                        RemoveEmployee();
+                        break;
+                    }
+                case 3:
+                    {
+                        PrintRegistry();
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Good bye!");
+                        Environment.Exit(1);
+                        break;
+                    }
 
-            employeeList.Add(anotherEmployee);
+                default:
+                    break;
+            }
+        }
 
-            foreach (var item in employeeList)
+        private static void PrintRegistry()
+        {
+            foreach (var item in registry.Employees)
             {
                 Console.WriteLine(item);
             }
 
-            // Remove employee
-            employeeList.Remove(newEmployee);
-
-            Console.WriteLine();
-            foreach (var item in employeeList)
+            // Print out log
+            foreach (var item in log.LogPosts)
             {
                 Console.WriteLine(item);
+            }
+        }
+
+        private static void RemoveEmployee()
+        {
+            Console.Write("SSN");
+            var ssn = Console.ReadLine();
+
+            
+            foreach (var item in registry.Employees)
+            {
+                if (item.SSN.Equals(ssn))
+                {
+                    registry.Employees.Remove(item);
+                     //// Add to log post
+                    log.Log(String.Format("Employee {0} removed from registry.",item.FirstName));
+                    break;
+                }
+                
             }
 
             
-
            
+        }
+
+        private static void AddEmployee()
+        {
+            Console.Write("First name:");
+            var firstName = Console.ReadLine();
+
+            Console.Write("Last name:");
+            var lastName = Console.ReadLine();
+
+            Console.Write("SSN:");
+            var ssn = Console.ReadLine();
+
+            Console.Write("Wage:");
+            var wage = Console.ReadLine();
+
+            // Add a new employee to the registry
+            var newEmployee = new Employee(firstName,lastName,ssn,wage);
+            var employeeList = registry.Employees;
+            employeeList.Add(newEmployee);
+
+            // Add to log post
+            log.Log(String.Format("New Employee, {0} added to registry.",newEmployee.FirstName));
+
+        }
+
+        private static void MenuPrint()
+        {
+            // Print the menu
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("1. Add employee");
+            stringBuilder.AppendLine("2. Remove employee");
+            stringBuilder.AppendLine("3. Print entire registry");
+            stringBuilder.AppendLine("4. Exit");
+            stringBuilder.AppendLine("Enter choice:");
+
+            Console.WriteLine(stringBuilder);
+
 
         }
     }
